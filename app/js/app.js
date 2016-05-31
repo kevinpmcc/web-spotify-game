2d558acccff70d68441f83a1d0fd8876198722a4
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-05-31 17:09:07 +0100

    removed all reference to ionic

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,4 @@
-angular.module('introGame.app', ['ionic',
-                                'ngAudio',
+angular.module('introGame.app', ['ngAudio',
                                 'introGame.routing',
                                 'introGame.albumController',
                                 'introGame.songController',
@@ -12,23 +11,4 @@ angular.module('introGame.app', ['ionic',
                                 'introGame.songFactory',
                                 'introGame.playFactory',
                                 'introGame.albumFactory',
-                                'introGame.albumFetcherService'])
-
-.run(function($ionicPlatform) {
-  console.log("Start of app");
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-});
+                                'introGame.albumFetcherService']);
