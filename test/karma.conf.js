ff5513e4c558dcc65bfd9df31f3b027466aa270f
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-11-19 12:20:24 +0000

    made changes to karma.conf.js to add heroku chrome

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -64,7 +64,13 @@ module.exports = function(config) {
       // start these browsers
       // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
       browsers: ['Chrome'],
-
+      
+      customLaunchers: {
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        },
 
       // Continuous Integration mode
       // if true, Karma captures browsers, runs the tests and exits
